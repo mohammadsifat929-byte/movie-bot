a8ebff6b251b940232fad42fcf95fe72a4a498d8
mohammadsifat929-byte <mohammadsifat929@gmail.com>
2026-05-29 11:28:42 +0600

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require("node-telegram-bot-api");
 
-const token = 'YOUR_BOT_TOKEN';
-const bot = new TelegramBot(token, { polling: true });
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, {
+  polling: true
+});
 
 bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "🎬 Movie Bot Working!");
+});
 
-    bot.sendMessage(
-        msg.chat.id,
-        "Bot Working ✅"
+bot.on("message", (msg) => {
+  if (msg.text === "movie") {
+    bot.sendDocument(
+      msg.chat.id,
+      "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4"
     );
-
+  }
 });
